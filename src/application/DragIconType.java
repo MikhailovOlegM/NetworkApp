a4ace3727cc4b20a793b0e9812f45fe3b4cbd611
Oleg <mikhailov.oleg.m@gmail.com>
2018-02-04 11:45:57 +0200

    init

---
+++ src/application/DragIconType.java
@@ -0,0 +1,13 @@
+package application;
+
+public enum DragIconType {
+	custom_object,
+	cubic_curve,	
+	red,
+	green,
+	blue,
+	black,
+	yellow,
+	purple,
+	grey
+}
---
+++ src/application/device/CustomDevice.java
@@ -0,0 +1,22 @@
+package application.device;
+
+public class CustomDevice {
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
